5980eba87fe30a34e99d9247d00da01a55518820
Shivani <63280793+shivanisajjan2420@users.noreply.github.com>
2023-03-30 21:26:05 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Minimum Integer/minimum-integer.java
@@ -0,0 +1,75 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class IntArray
+{
+    public static int[] input(BufferedReader br, int n) throws IOException
+    {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++)
+            a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int N;
+            N = Integer.parseInt(br.readLine());
+            
+            
+            int[] A = IntArray.input(br, N);
+            
+            Solution obj = new Solution();
+            int res = obj.minimumInteger(N, A);
+            
+            System.out.println(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public static int minimumInteger(int N, int[] A) {
+        // code here
+        long S=0;
+        long val=Integer.MAX_VALUE;
+        for(int i=0;i<N;i++){
+            S+=A[i];
+        }
+        for(int j=0;j<N;j++){
+            long X=A[j];
+            if( S <= N*X){
+                val=Math.min(X,val);
+            }
+        }
+        return (int)val;
+    }
+}
+        
